8f0f0ce95f95c4d41b9a17b2672ce1419cfa322e
rusairdrop <rusairdrop@gmail.com>
2021-08-30 12:29:02 +0300

    роутинг компонентов

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Create from "./components/Create";
+import Main from "./components/Main";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Error from "./components/Error";
 
 function App() {
   return (
     <div className="main">
-    
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exect path='/' component={Main}/>
+          <Route path='/about' component={About}/>
+          <Route path='/create' component={Create}/>
+          <Route exact path='/note/' component={Note}/>
+          <Route exact path='/note/:noteURL' component={Note}/>
+          <Route component={Error}/>
+        </Switch>
+      </Router>
+      <Footer/>
     </div>
   );
 }
