b0f17c1a8f024fc1e668252bdd93e647df590e9a
rusairdrop <rusairdrop@gmail.com>
2021-08-30 18:30:51 +0300

    + Main компонент + Header компонент

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+import {NavLink} from "react-router-dom";
 import React from 'react';
 
 const Header = () => {
   return (
     <div>
-      
+      <nav>
+        <ul>
+          <li><NavLink exact className='' to='/'>Home</NavLink></li>
+          <li><NavLink className='' to='/note'>Note</NavLink></li>
+          <li><NavLink className='' to='/create'>Create</NavLink></li>
+          <li><NavLink className='' to='/about'>About</NavLink></li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Main = () => {
   return (
     <div>
-      
+      <div>
+        <a href='/create'>Создать Note</a>
+      </div>
+      <div>
+        <a href='/note'>Посмотреть Note</a>
+      </div>
     </div>
   );
 };
