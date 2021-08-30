ea0b4da426fcf083687cf6e046fcdac203d5a158
rusairdrop <rusairdrop@gmail.com>
2021-08-30 12:50:15 +0300

    + компонент Create + файл с параметрами проекта env.json

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import env from '../env.json';
 
 const Create = () => {
+  
+  const [url, setUrl] = useState('');
+  
+  const sendData = (obj) => {
+    fetch(env.urlBackend, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(obj)
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        if (response.result) {
+          setUrl(env.url + '/' + response.url);
+        }
+      })
+  }
+  
+  const loadDataFromForm = (event) => {
+    event.preventDefault();
+    let note = event.target.elements.note.value;
+    note = note.trim();
+    if (note === '') {
+      alert('Заполните поля');
+      return false;
+    }
+    sendData({'note': note});
+  }
+  
   return (
     <div>
-      
+      <form onSubmit={loadDataFromForm}>
+        <label htmlFor="">Введите заметку</label>
+        <textarea name="note"
+                  id="note"
+                  cols="30"
+                  rows="10"
+                  defaultValue='Test'/>
+        <button type='submit'>Создать</button>
+      </form>
     </div>
   );
 };
