8505928c2dfdd7a07f1786c5646e13a95883ab8a
rusairdrop <rusairdrop@gmail.com>
2021-09-04 20:27:05 +0300

    fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__copyright">
-        <div>Copyright &copy; 2021.  Made with &#10084; by Galyshev.pro</div>
+        <div>Copyright &copy; 2021. Made with &#10084; by Galyshev.pro</div>
       </div>
-  
+      
       <div className="footer__social">
         <a target="_blank" rel="noreferrer" href="https://t.me" className="footer__social-link">Telegram</a>
         <a target="_blank" rel="noreferrer" href="https://twitter.com" className="footer__social-link">Twitter</a>
